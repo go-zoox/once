1ac324f84379f3a0bc22692a9010884b0f7696fc
Zero <tobewhatwewant@gmail.com>
2024-01-08 17:22:50 +0800

    fix: Get when the same name, should panic

--- get.go
+++ get.go
@@ -1,10 +1,17 @@
 package once
 
+import "fmt"
+
 // Get ...
 func Get[T any](name string, fn func() T) T {
 	v := global.Get(name, func() any {
 		return fn()
 	})
 
-	return v.(T)
+	vt, ok := v.(T)
+	if !ok {
+		panic(fmt.Sprintf("once.Get: register the same name(%s) with different type", name))
+	}
+
+	return vt
 }
